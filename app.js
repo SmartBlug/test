16216b1c433bb5de8b0f4a4062052029446634ec
SmartBlug <SmartBlug@SmartExtension.com>
2020-07-30 19:42:56 +0200

    version

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+const version = require('./package.json').version;
+
 app.get('/', function (req, res) {
-  res.send('Hello World 4!')
+  res.send('Hello World 4 - version '+version)
 })
 
 app.listen(3000, function () {

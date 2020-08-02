b2f86549f724ea1db852dba1a31b8adb936a665b
SmartBlug <SmartBlug@SmartExtension.com>
2020-08-02 19:48:50 +0200

    pre-commit 3

--- pre-commit.js
+++ pre-commit.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
-const package = require('../../package.json')
+const package = require(__dirname+'/package.json');
 
 package.build = parseInt(execSync("git log master --pretty=oneline | wc -l").toString())+1;
-fs.writeFileSync('../../package.json', JSON.stringify(package,null,"  "));
+fs.writeFileSync(__dirname+'/package.json', JSON.stringify(package,null,"  "));
 execSync("git add package.json");

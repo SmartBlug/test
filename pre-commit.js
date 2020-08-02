4e3fa97c87dd81d30f48e7ab490163182d9a253d
SmartBlug <SmartBlug@SmartExtension.com>
2020-08-02 20:11:49 +0200

    test

--- pre-commit.js
+++ pre-commit.js
@@ -1,5 +1,8 @@
 // Create a file .git/hooks/pre-commit with :
+//
 // node pre-commit.js
+//
+// and keep this file in your root folder. This will align the build number of your package with git
 
 const fs = require('fs');
 const { execSync } = require('child_process');
@@ -8,3 +11,5 @@ const package = require(__dirname+'/package.json');
 package.build = parseInt(execSync("git log master --pretty=oneline | wc -l").toString())+1;
 fs.writeFileSync(__dirname+'/package.json', JSON.stringify(package,null,"  "));
 execSync("git add package.json");
+
+// sed 's/^  "build": [0-9]*,/  "build": '"12"',/' package.json
